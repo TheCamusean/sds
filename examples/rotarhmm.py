65e1736eb27dcd8829f5bff452fc09ccab3e0ae2
Julen Urain De Jesus <urain@ias.informatik.tu-darmstadt.de>
2019-09-18 17:59:44 +0200

    trial2

--- examples/rotarhmm.py
+++ examples/rotarhmm.py
@@ -48,37 +48,37 @@ lls2 = arhmm2.em(x, nb_iter=100, prec=1e-8, verbose=True)
 
 print("true_ll=", true_ll, "hmm_ll=", lls[-1], "hmm_ll2=", lls2[-1])
 
-# plt.figure(figsize=(5, 5))
-# plt.plot(np.ones(len(lls)) * true_ll, '-r')
-# plt.plot(lls)
-# plt.show()
-#
-# _, arhmm_z = arhmm.viterbi(x)
-# _seq = npr.choice(len(x))
-# arhmm.permute(permutation(true_z[_seq], arhmm_z[_seq], K1=3, K2=3))
-#
-# _, arhmm_z = arhmm.viterbi(x[_seq])
-#
-# plt.figure(figsize=(8, 4))
-# plt.subplot(211)
-# plt.imshow(true_z[_seq][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
-# plt.xlim(0, len(x[_seq]))
-# plt.ylabel("$z_{\\mathrm{true}}$")
-# plt.yticks([])
-#
-# plt.subplot(212)
-# plt.imshow(arhmm_z[0][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
-# plt.xlim(0, len(x[_seq]))
-# plt.ylabel("$z_{\\mathrm{inferred}}$")
-# plt.yticks([])
-# plt.xlabel("time")
-#
-# plt.tight_layout()
-# plt.show()
-#
-# arhmm_x = arhmm.mean_observation(x)
-#
-# plt.figure(figsize=(8, 4))
-# plt.plot(x[_seq] + 10 * np.arange(arhmm.dm_obs), '-k', lw=2)
-# plt.plot(arhmm_x[_seq] + 10 * np.arange(arhmm.dm_obs), '-', lw=2)
-# plt.show()
+plt.figure(figsize=(5, 5))
+plt.plot(np.ones(len(lls)) * true_ll, '-r')
+plt.plot(lls)
+plt.show()
+
+_, arhmm_z = arhmm.viterbi(x)
+_seq = npr.choice(len(x))
+arhmm.permute(permutation(true_z[_seq], arhmm_z[_seq], K1=3, K2=3))
+
+_, arhmm_z = arhmm.viterbi(x[_seq])
+
+plt.figure(figsize=(8, 4))
+plt.subplot(211)
+plt.imshow(true_z[_seq][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
+plt.xlim(0, len(x[_seq]))
+plt.ylabel("$z_{\\mathrm{true}}$")
+plt.yticks([])
+
+plt.subplot(212)
+plt.imshow(arhmm_z[0][None, :], aspect="auto", cmap=cmap, vmin=0, vmax=len(colors) - 1)
+plt.xlim(0, len(x[_seq]))
+plt.ylabel("$z_{\\mathrm{inferred}}$")
+plt.yticks([])
+plt.xlabel("time")
+
+plt.tight_layout()
+plt.show()
+
+arhmm_x = arhmm.mean_observation(x)
+
+plt.figure(figsize=(8, 4))
+plt.plot(x[_seq] + 10 * np.arange(arhmm.dm_obs), '-k', lw=2)
+plt.plot(arhmm_x[_seq] + 10 * np.arange(arhmm.dm_obs), '-', lw=2)
+plt.show()
--- sds/observations.py
+++ sds/observations.py
@@ -463,7 +463,7 @@ class AutoRegressiveGaussianObservation:
         return mean
 
 
-class RotAutoRegressiveGaussianObservation:
+class RotAutoRegressiveGaussianObservation2:
 
     def __init__(self, nb_states, dm_obs, n_rot, prior, dm_act=0, reg=1e-8):
         self.nb_states = nb_states
@@ -712,6 +712,204 @@ class RotAutoRegressiveGaussianObservation:
         #         _cov[k] = _cov[i]
         #
         # self.cov = _cov
+
+    def smooth(self, gamma, x, u):
+        mean = []
+        for _x, _u, _gamma in zip(x, u, gamma):
+            _mu = np.zeros((len(_x) - 1, self.nb_states, self.dm_obs))
+            for k in range(self.nb_states):
+                _mu[:, k, :] = self.mean(k, _x[:-1, :], _u[:-1, :self.dm_act])
+            mean.append(np.einsum('nk,nkl->nl', _gamma[1:, ...], _mu))
+        return mean
+
+
+class RotAutoRegressiveGaussianObservation:
+
+    def __init__(self, nb_states, dm_obs, n_rot, prior, dm_act=0, reg=1.e-16):
+        self.nb_states = nb_states
+        self.nb_lds = int(nb_states / n_rot)
+        self.dm_obs = dm_obs
+        self.dm_act = dm_act
+
+        self.prior = prior
+        self.reg = reg
+
+        self.n_rot = n_rot
+        self.rot_lds = np.zeros([self.nb_states, 2])
+
+
+        self.A = np.zeros((self.nb_states, self.dm_obs, self.dm_obs))
+        self.B = np.zeros((self.nb_states, self.dm_obs, self.dm_act))
+        self.c = np.zeros((self.nb_states, self.dm_obs))
+
+        for k in range(self.nb_states):
+            self.A[k, ...] = .95 * random_rotation(self.dm_obs)
+            self.B[k, ...] = npr.randn(self.dm_obs, self.dm_act)
+            self.c[k, :] = npr.randn(self.dm_obs)
+
+        self._sqrt_cov = npr.randn(self.nb_states, self.dm_obs, self.dm_obs)
+
+        self.T = np.zeros((self.n_rot, self.dm_obs, self.dm_obs))
+        self.T_inv = np.zeros((self.n_rot, self.dm_obs, self.dm_obs))
+        for k in range(self.n_rot):
+            # self.T[k, ...] = .95 * random_rotation(self.dm_obs)
+            self.T[k, ...] = np.array([[1, 0], [0, 1]])
+            # self.T[k, ...] = np.array([[0, -1], [1, 0]])
+            self.T_inv[k, ...] = np.linalg.inv(self.T[k, ...])
+
+    @property
+    def params(self):
+        return self.A, self.B, self.c, self._sqrt_cov
+
+    @params.setter
+    def params(self, value):
+        self.A, self.B, self.c, self._sqrt_cov = value
+
+    def mean(self, z, x, u):
+        return np.einsum('kh,...h->...k', self.A[z, ...], x) +\
+               np.einsum('kh,...h->...k', self.B[z, ...], u) + self.c[z, :]
+
+    @property
+    def cov(self):
+        return np.matmul(self._sqrt_cov, np.swapaxes(self._sqrt_cov, -1, -2))
+
+    @cov.setter
+    def cov(self, value):
+        self._sqrt_cov = np.linalg.cholesky(value + self.reg * np.eye(self.dm_obs))
+
+    def sample(self, z, x, u, stoch=True):
+        if stoch:
+            return mvn(self.mean(z, x, u), cov=self.cov[z, ...]).rvs()
+        else:
+            return self.mean(z, x, u)
+
+    def initialize(self, x, u, **kwargs):
+        localize = kwargs.get('localize', True)
+
+        Ts = [_x.shape[0] for _x in x]
+        if localize:
+            from sklearn.cluster import KMeans
+            km = KMeans(self.nb_states, random_state=1)
+            km.fit((np.vstack(x)))
+            zs = np.split(km.labels_, np.cumsum(Ts)[:-1])
+            zs = [z[:-1] for z in zs]
+        else:
+            zs = [npr.choice(self.nb_states, size=T - 1) for T in Ts]
+
+        _cov = np.zeros((self.nb_states, self.dm_obs, self.dm_obs))
+        for k in range(self.nb_states):
+            ts = [np.where(z == k)[0] for z in zs]
+            xs = [np.hstack((_x[t, :], _u[t, :])) for t, _x, _u in zip(ts, x, u)]
+            ys = [_x[t + 1, :] for t, _x in zip(ts, x)]
+
+            coef_, intercept_, sigma = linear_regression(xs, ys)
+            self.A[k, ...] = coef_[:, :self.dm_obs]
+            self.B[k, ...] = coef_[:, self.dm_obs:]
+            self.c[k, :] = intercept_
+            _cov[k, ...] = sigma
+
+        self.cov = _cov
+
+    def permute(self, perm):
+        self.A = self.A[perm, ...]
+        self.B = self.B[perm, ...]
+        self.c = self.c[perm, :]
+        self._sqrt_cov = self._sqrt_cov[perm, ...]
+
+    def log_prior(self):
+        lp = 0.
+        if self.prior:
+            for k in range(self.nb_states):
+                coef_ = np.column_stack((self.A[k, ...], self.B[k, ...], self.c[k, ...])).flatten()
+                lp += mvn(mean=self.prior['mu0'] * np.zeros((coef_.shape[0], )),
+                          cov=self.prior['sigma0'] * np.eye(coef_.shape[0])).logpdf(coef_)\
+                      + invw(self.prior['nu0'], self.prior['psi0'] * np.eye(self.dm_obs)).logpdf(self.cov[k, ...])
+        return lp
+
+    def log_likelihood(self, x, u):
+        loglik = []
+        for _x, _u in zip(x, u):
+            _loglik = np.column_stack([multivariate_normal_logpdf(_x[1:, :], self.mean(k, _x[:-1, :], _u[:-1, :self.dm_act]), self.cov[k])
+                                       for k in range(self.nb_states)])
+            loglik.append(_loglik)
+        return loglik
+
+    def mstep(self, gamma, x, u, **kwargs):
+        xs, ys, ws = [], [], []
+        for _x, _u, _w in zip(x, u, gamma):
+            xs.append(np.hstack((_x[:-1, :], _u[:-1, :self.dm_act], np.ones((_x.shape[0] - 1, 1)))))
+            ys.append(_x[1:, :])
+            ws.append(_w[1:, :])
+
+        _cov = np.zeros((self.nb_states, self.dm_obs, self.dm_obs))
+        for k in range(self.nb_states):
+            coef_, sigma = linear_regression(Xs=np.vstack(xs), ys=np.vstack(ys),
+                                             weights=np.vstack(ws)[:, k], fit_intercept=False,
+                                             **self.prior)
+            self.A[k, ...] = coef_[:, :self.dm_obs]
+            self.B[k, ...] = coef_[:, self.dm_obs:self.dm_obs + self.dm_act]
+            self.c[k, ...] = coef_[:, -1]
+            _cov[k, ...] = sigma
+
+        # usage = sum([_gamma.sum(0) for _gamma in gamma])
+        # unused = np.where(usage < 1)[0]
+        # used = np.where(usage > 1)[0]
+        # if len(unused) > 0:
+        #     for k in unused:
+        #         i = npr.choice(used)
+        #         self.A[k] = self.A[i] + 0.01 * npr.randn(*self.A[i].shape)
+        #         self.B[k] = self.B[i] + 0.01 * npr.randn(*self.B[i].shape)
+        #         self.c[k] = self.c[i] + 0.01 * npr.randn(*self.c[i].shape)
+        #         _cov[k] = _cov[i]
+
+        self.cov = _cov
+
+    # def mstep(self, gamma, x, u, **kwargs):
+    #     xs, ys, ws = [], [], []
+    #     for _x, _u, _w in zip(x, u, gamma):
+    #         xs.append(np.hstack((_x[:-1, :], _u[:-1, :self.dm_act], np.ones((_x.shape[0] - 1, 1)))))
+    #         ys.append(_x[1:, :])
+    #         ws.append(_w[1:, :])
+    #
+    #     _J_diag = np.concatenate((self.reg * np.ones(self.dm_obs),
+    #                               self.reg * np.ones(self.dm_act),
+    #                               self.reg * np.ones(1)))
+    #     _J = np.tile(np.diag(_J_diag)[None, :, :], (self.nb_states, 1, 1))
+    #     _h = np.zeros((self.nb_states, self.dm_obs + self.dm_act + 1, self.dm_obs))
+    #
+    #     for _x, _y, _w in zip(xs, ys, ws):
+    #         for k in range(self.nb_states):
+    #             wx = _x * _w[:, k:k + 1]
+    #             _J[k] += np.dot(wx.T, _x)
+    #             _h[k] += np.dot(wx.T, _y)
+    #
+    #     mu = np.linalg.solve(_J, _h)
+    #     self.A = np.swapaxes(mu[:, :self.dm_obs, :], 1, 2)
+    #     self.B = np.swapaxes(mu[:, self.dm_obs:self.dm_obs + self.dm_act, :], 1, 2)
+    #     self.c = mu[:, -1, :]
+    #
+    #     sqerr = np.zeros((self.nb_states, self.dm_obs, self.dm_obs))
+    #     weight = self.reg * np.ones(self.nb_states)
+    #     for _x, _y, _w in zip(xs, ys, ws):
+    #         yhat = np.matmul(_x[None, :, :], mu)
+    #         resid = _y[None, :, :] - yhat
+    #         sqerr += np.einsum('tk,kti,ktj->kij', _w, resid, resid)
+    #         weight += np.sum(_w, axis=0)
+    #
+    #     _cov = sqerr / weight[:, None, None]
+    #
+    #     usage = sum([_gamma.sum(0) for _gamma in gamma])
+    #     unused = np.where(usage < 1)[0]
+    #     used = np.where(usage > 1)[0]
+    #     if len(unused) > 0:
+    #         for k in unused:
+    #             i = npr.choice(used)
+    #             self.A[k] = self.A[i] + 0.01 * npr.randn(*self.A[i].shape)
+    #             self.B[k] = self.B[i] + 0.01 * npr.randn(*self.B[i].shape)
+    #             self.c[k] = self.c[i] + 0.01 * npr.randn(*self.c[i].shape)
+    #             _cov[k] = _cov[i]
+    #
+    #     self.cov = _cov
 
     def smooth(self, gamma, x, u):
         mean = []
